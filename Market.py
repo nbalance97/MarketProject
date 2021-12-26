7772d7ea923bade857e89f886577a65fef70e071
nbalance97 <nbalance__@kyonggi.ac.kr>
2021-12-27 00:59:17 +0900

    feat: Item 추가 파트 구현

--- Market.py
+++ Market.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Market.sqlite3'
@@ -11,6 +12,27 @@ db.metadata.clear()
 def index():
     from Model import Post
     return render_template('itemlist.html', posts=Post.query.all())
+
+@app.route('/item', methods=['GET', 'POST'])
+def createItem():
+    from Model import Post
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+        address = request.form['address']
+        contents = request.form['contents']
+        user_id = 1
+        db.session.add(Post(
+            title=title, 
+            price=int(price), 
+            address=address, 
+            contents=contents,
+            author_id=user_id
+        ))
+        db.session.commit()
+        return render_template('itemlist.html')
+    else:
+        return render_template('itemadd.html')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
